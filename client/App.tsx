11b83345f59bcfcd324cdf979a7dac842d61daad
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-17 17:22:40 -0500

    Added Apollo Client

--- client/App.tsx
+++ client/App.tsx
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi!</Text>
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Text>Hi!</Text>
+      </View>
+    </ApolloProvider>
   );
 }
 
