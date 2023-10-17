648ab21cda1acb4db44242d88da8e6c9e900d100
Henry Nguyen <nguyenhenry682@gmail.com>
2023-10-17 18:31:12 -0500

    Added Dashboard component with hooks

--- client/App.tsx
+++ client/App.tsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import Dashboard from './screens/Dashboard';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
@@ -10,7 +11,7 @@ export default function App() {
   return (
     <ApolloProvider client={client}>
       <View style={styles.container}>
-        <Text>Hi!</Text>
+        <Dashboard />
       </View>
     </ApolloProvider>
   );
